6e7186086a5870a94e1d3bf9834bda72006db1e5
tianchen2019 <18821690169@163.com>
2020-05-20 19:33:44 +0800

    Update find_differential_express_gene.r

--- find_differential_express_gene.r
+++ find_differential_express_gene.r
@@ -21,7 +21,7 @@ rownames(results1)<-rownames(data_analysis)
 
 ### 三组之间比较的code
 gene_expression<-read.table(file="genes.fpkm_tracking_bulk_rnaseq_mhsc",header=T)
-rownames(gene_expression)<-gene_expression$$tracking_id
+rownames(gene_expression)<-gene_expression$tracking_id
 high<-gene_expression[,c("HSC_MH2_B6J_FPKM","HSC_MH2_B6J_FPKM","HSC_FH2_B6J_FPKM","HSC_FH1_B6J_FPKM")]
 low<-gene_expression[,c("HSC_ML1_B6J_FPKM","HSC_ML2_B6J_FPKM","HSC_FL1_B6J_FPKM","HSC_FL2_B6J_FPKM")]
 neg<-gene_expression[,c("HSC_MN1_B6J_FPKM","HSC_MN2_B6J_FPKM","HSC_FN1_B6J_FPKM","HSC_FN2_B6J_FPKM")]
@@ -31,6 +31,7 @@ data3<-neg
 gene1<- intersect(rownames(data1),rownames(data2))
 gene_final<-intersect(gene1,rownames(data3))
 data_analysis<-cbind(data1[gene_final,],data2[gene_final,],data3[gene_final,])
+
 myFun1 <- function(x){
   x = as.numeric(x)
   v1 = x[1:dim(data1)[2]]
@@ -88,10 +89,31 @@ data3_sig_gene1<-rownames(results)[results$p_value2<0.05 & results$log2fc1<0]
 data3_sig_gene2<-rownames(results)[results$p_value3<0.05 & results$log2fc3<0]
 data3_sig_gene<-unique(c(data3_sig_gene1,data3_sig_gene2))
 
-list_3_class<-list("high"=setdiff(data1_sig_gene,c(data2_sig_gene,data3_sig_gene)),
+list_3_class_2<-list("high"=setdiff(data1_sig_gene,c(data2_sig_gene,data3_sig_gene)),
                    "low"=setdiff(data2_sig_gene,c(data1_sig_gene,data3_sig_gene)),
                    "neg"=setdiff(data3_sig_gene,c(data1_sig_gene,data2_sig_gene)))
 
+#### select fc
+high_fc_mean<-apply(results[list_3_class_2$high,c(4,7)],1,mean)
+high_fc_mean<-data.frame("gene"=list_3_class_2$high,"mean_fc"=high_fc_mean)
+rownames(high_fc_mean)<-list_3_class_2$high
+
+low_fc_mean<-apply(abs(results[list_3_class_2$low,c(4,10)]),1,mean)
+low_fc_mean<-data.frame("gene"=list_3_class_2$low,"mean_fc"=low_fc_mean)
+rownames(low_fc_mean)<-list_3_class_2$low
+
+neg_fc_mean<-apply(abs(results[list_3_class_2$neg,c(7,10)]),1,mean)
+neg_fc_mean<-data.frame("gene"=list_3_class_2$neg,"mean_fc"=neg_fc_mean)
+rownames(neg_fc_mean)<-list_3_class_2$neg
+
+####rank
+high_gene_rank<-arrange(high_fc_mean,desc(mean_fc))
+low_gene_rank<-arrange(low_fc_mean,desc(mean_fc))
+neg_gene_rank<-arrange(neg_fc_mean,desc(mean_fc))
+
+list_3_class_rank<-list("high"=high_gene_rank$gene,
+                        "low"=low_gene_rank$gene,
+                        "neg"=neg_gene_rank$gene)
 
 ####high+low vs neg sig gene
 myFun4 <- function(x){
@@ -112,5 +134,62 @@ rownames(results2)<-rownames(data_analysis)
 data1_2_sig_gene<-rownames(results2)[p_value4<0.05 & log2fc4>0]
 data3_sig_gene<-rownames(results2)[p_value4<0.05 & log2fc4<0]
 list_2_class<-list("high_low"=data1_2_sig_gene,"neg"=data3_sig_gene)
+high_low_sig_gene_mt<-data.frame("gene"=list_2_class$high_low,"fc"=results2[list_2_class$high_low,3])
+high_low_gene_rank<-arrange(high_low_sig_gene_mt,desc(fc))
+
+neg_sig_gene_mt<-data.frame("gene"=list_2_class$neg,"fc"=abs(results2[list_2_class$neg,3]))
+neg_2_class_gene_rank<-arrange(neg_sig_gene_mt,desc(fc))
+list_2_class_rank<-list("high_low"=high_low_gene_rank$gene,
+                        "neg"=neg_2_class_gene_rank$gene)
+
+##### gene featureplot
+load("/Share2/home/lanxun11/tianchen_rotation/young_hoxb_analysis/result_move_batch/all_cell_move_batch4.rdata")
+plot_high_gene<-intersect(list_3_class_rank$high,rownames(list.integrated@assays$RNA))
+plot_low_gene<-intersect(list_3_class_rank$low,rownames(list.integrated@assays$RNA))
+plot_neg_gene<-intersect(list_3_class_rank$neg,rownames(list.integrated@assays$RNA))
+
+plot_list_pos=list()
+for(j in 1:length(plot_high_gene[1:20])){
+	p1<-FeaturePlot(object = list.integrated, dims = c(1,2),features = plot_high_gene[j],reduction ="tsne",
+    cols = c("lightgrey", "red"),pt.size=1)
+    plot_list_pos[[j]]=p1
+}
+
+for (j in 1:length(plot_list_pos)){
+  file_name1=paste(j,"_high",".png",sep="")
+  png(file_name1)
+  print(plot_list_pos[[j]])
+  dev.off()
+}
+
+###low_plot
+plot_list_pos=list()
+for(j in 1:length(plot_low_gene[1:20])){
+	p1<-FeaturePlot(object = list.integrated, dims = c(1,2),features = plot_low_gene[j],reduction ="tsne",
+    cols = c("lightgrey", "red"),pt.size=1)
+    plot_list_pos[[j]]=p1
+}
+
+for (j in 1:length(plot_list_pos)){
+  file_name1=paste(j,"_low",".png",sep="")
+  png(file_name1)
+  print(plot_list_pos[[j]])
+  dev.off()
+}
+#### neg
+plot_list_pos=list()
+for(j in 1:length(plot_neg_gene[1:20])){
+	p1<-FeaturePlot(object = list.integrated, dims = c(1,2),features = plot_neg_gene[j],reduction ="tsne",
+    cols = c("lightgrey", "red"),pt.size=1)
+    plot_list_pos[[j]]=p1
+}
+
+for (j in 1:length(plot_list_pos)){
+  file_name1=paste(j,"_neg",".png",sep="")
+  png(file_name1)
+  print(plot_list_pos[[j]])
+  dev.off()
+}
+
 
 
